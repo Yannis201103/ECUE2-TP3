b4fe22259afc16c0e44bd7fe98f2cf8d21f5af73
Mouchard Yannis <yannis.mouchard@gmail.com>
2026-09-18 15:23:06 +0200

    Exercice 7

---
+++ src/net/lecnam/ecue2/tp3/Dessin.java
@@ -0,0 +1,12 @@
+package net.lecnam.ecue2.tp3;
+
+public class Dessin {
+
+    private static Exercice2.Rectangle[] rectangles;
+    private static int nombreRectangles;
+
+    public Dessin() {
+        rectangles = new Exercice2.Rectangle[10];
+        nombreRectangles = 0;
+    }
+}
--- src/net/lecnam/ecue2/tp3/Exec.java
+++ src/net/lecnam/ecue2/tp3/Exec.java
@@ -1,9 +1,6 @@
 package net.lecnam.ecue2.tp3;
 
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
 public class Exec  {
     public static void main(String[] args) {
         Exercice1.Point point = new Exercice1.Point(6,7);
